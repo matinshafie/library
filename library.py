3ca1e99c3e49de088d1183c784c5b0e2d0f5a024
matin shafie <matinshafie88@gmail.com>
2025-08-02 23:28:35 +0330

    added search operations for finding stuff

--- library.py
+++ library.py
@@ -76,3 +76,23 @@ class Library:
         query = "SELECT 1 FROM authors WHERE author_id=%s LIMIT 1"
         self.cursor.execute(query, (author_id,))
         return self.cursor.fetchone() is not None
+
+    def search_book_by_id(self, book_id: int) -> tuple:
+        query = "SELECT * FROM books WHERE book_id=%s"
+        self.cursor.execute(query, (book_id,))
+        return self.cursor.fetchall()
+
+    def search_author_by_id(self, author_id: int) -> tuple:
+        query = "SELECT * FROM authors WHERE author_id=%s"
+        self.cursor.execute(query, (author_id,))
+        return self.cursor.fetchone()
+
+    def search_by_book_name(self, book_name: str) -> list[tuple]:
+        query = "SELECT * FROM books WHERE title REGEXP %s"
+        self.cursor.execute(query, (book_name,))
+        return self.cursor.fetchall()
+
+    def search_by_author_name(self, author_name: str) -> list[tuple]:
+        query = "SELECT * FROM authors WHERE name REGEXP %s"
+        self.cursor.execute(query, (author_name,))
+        return self.cursor.fetchall()
