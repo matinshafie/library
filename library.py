b519ab0540a239631fa226816d62faf03368dbe5
matinshafie <matinshafie88@gmail.com>
2025-08-14 16:13:05 +0330

    added remove and add user functions

--- library.py
+++ library.py
@@ -15,11 +15,11 @@ def get_connection() -> (PooledMySQLConnection | MySQLConnectionAbstract):
         )
 class Library:
     def __init__(self):
-        self.connection = get_connection()
-        self.cursor = self.connection.cursor()
-        self.cursor.execute("CREATE DATABASE IF NOT EXISTS library")
-        self.cursor.execute(f'USE library')
-        self.cursor.execute(
+        self.__connection = get_connection()
+        self.__cursor = self.__connection.cursor()
+        self.__cursor.execute("CREATE DATABASE IF NOT EXISTS library")
+        self.__cursor.execute(f'USE library')
+        self.__cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS books (
                 book_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -30,7 +30,7 @@ class Library:
             )
         """
         )
-        self.cursor.execute(
+        self.__cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS authors (
                 author_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -41,71 +41,71 @@ class Library:
 
 
     def add_book(self, title: str, author_id: int, published_year: int):
-        self.cursor.execute(
+        self.__cursor.execute(
             "INSERT INTO books (title, author_id, published_year,added_date) VALUES (%s, %s, %s,CURDATE())",
             (title, author_id, published_year),
         )
-        self.connection.commit()
+        self.__connection.commit()
 
     def add_author(self, name: str):
-        self.cursor.execute("INSERT INTO authors (name) VALUES (%s)", (name,))
-        self.connection.commit()
+        self.__cursor.execute("INSERT INTO authors (name) VALUES (%s)", (name,))
+        self.__connection.commit()
 
     def remove_book(self, book_id: int):
         query = "DELETE FROM books WHERE book_id=%s"
-        self.cursor.execute(query, (book_id,))
-        self.connection.commit()
+        self.__cursor.execute(query, (book_id,))
+        self.__connection.commit()
 
     def remove_author(self, author_id: int):
         query = "DELETE FROM authors WHERE author_id=%s"
-        self.cursor.execute(query, (author_id,))
-        self.connection.commit()
+        self.__cursor.execute(query, (author_id,))
+        self.__connection.commit()
 
     def get_books(self) -> list[tuple]:
-        self.cursor.execute("SELECT * FROM books")
-        results = self.cursor.fetchall()
+        self.__cursor.execute("SELECT * FROM books")
+        results = self.__cursor.fetchall()
 
         return results
 
     def get_authors(self) -> list[tuple]:
-        self.cursor.execute("SELECT * FROM authors")
-        results = self.cursor.fetchall()
+        self.__cursor.execute("SELECT * FROM authors")
+        results = self.__cursor.fetchall()
 
         return results
 
     def book_existence(self, book_id: int) -> bool:
         query = "SELECT 1 FROM books WHERE book_id=%s LIMIT 1"
-        self.cursor.execute(query, (book_id,))
-        return self.cursor.fetchone() is not None
+        self.__cursor.execute(query, (book_id,))
+        return self.__cursor.fetchone() is not None
 
     def author_existence(self, author_id: int) -> bool:
         query = "SELECT 1 FROM authors WHERE author_id=%s LIMIT 1"
-        self.cursor.execute(query, (author_id,))
-        return self.cursor.fetchone() is not None
+        self.__cursor.execute(query, (author_id,))
+        return self.__cursor.fetchone() is not None
 
     def search_book_by_id(self, book_id: int) -> tuple:
         query = "SELECT * FROM books WHERE book_id=%s"
-        self.cursor.execute(query, (book_id,))
-        return self.cursor.fetchall()
+        self.__cursor.execute(query, (book_id,))
+        return self.__cursor.fetchall()
 
     def search_author_by_id(self, author_id: int) -> tuple:
         query = "SELECT * FROM authors WHERE author_id=%s"
-        self.cursor.execute(query, (author_id,))
-        return self.cursor.fetchone()
+        self.__cursor.execute(query, (author_id,))
+        return self.__cursor.fetchone()
 
     def search_by_book_title(self, book_name: str) -> list[tuple]:
         if not book_name:
             return []
         query = "SELECT * FROM books WHERE title REGEXP %s"
-        self.cursor.execute(query, (book_name,))
-        return self.cursor.fetchall()
+        self.__cursor.execute(query, (book_name,))
+        return self.__cursor.fetchall()
 
     def search_by_author_name(self, author_name: str) -> list[tuple]:
         query = "SELECT * FROM authors WHERE name REGEXP %s"
         if not author_name:
             return []
-        self.cursor.execute(query, (author_name,))
-        return self.cursor.fetchall()
+        self.__cursor.execute(query, (author_name,))
+        return self.__cursor.fetchall()
 
     def search_book_by_author_name(self, author_name: str) -> list[tuple]:
         author_ids = tuple(
@@ -115,27 +115,27 @@ class Library:
             return []
         placeholders = ",".join(["%s"] * len(author_ids))
         query = f"SELECT * FROM books WHERE author_id IN ({placeholders})"
-        self.cursor.execute(query, author_ids)
-        return self.cursor.fetchall()
+        self.__cursor.execute(query, author_ids)
+        return self.__cursor.fetchall()
 
     def search_book_by_published_year(self, published_year: int) -> list[tuple]:
         query = "SELECT * FROM books WHERE published_year=%s"
-        self.cursor.execute(query, (published_year,))
-        return self.cursor.fetchall()
+        self.__cursor.execute(query, (published_year,))
+        return self.__cursor.fetchall()
 
     def add_user_type(self,user_type:str,limit:int):
         query="INSERT INTO user_types (type,limit_borrow) VALUES (%s,%s)"
-        self.cursor.execute(query,(user_type,limit))
-        self.connection.commit()
+        self.__cursor.execute(query,(user_type,limit))
+        self.__connection.commit()
 
 class UserOperations:
     def __init__(self):
-        self.connection=get_connection()
-        self.cursor=self.connection.cursor()
-        self.cursor.execute("CREATE DATABASE IF NOT EXISTS users")
-        self.cursor.execute(f'USE users')
+        self.__connection=get_connection()
+        self.__cursor=self.__connection.cursor()
+        self.__cursor.execute("CREATE DATABASE IF NOT EXISTS users")
+        self.__cursor.execute(f'USE users')
         
-        self.cursor.execute(
+        self.__cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS user_types (
                 type_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -144,11 +144,16 @@ class UserOperations:
             )
         """
         )
-        self.cursor.execute(
+        self.__cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS users (
                 user_id INT PRIMARY KEY AUTO_INCREMENT,
-                type_id VARCHAR(50) NOT NULL,
+                first_name VARCHAR(50) NOT NULL,
+                last_name VARCHAR(50) NOT NULL,
+                user_name VARCHAR(50) NOT NULL,
+                type_id INT NOT NULL,
+                password VARCHAR(8) NOT NULL,
+                age INT NOT NULL,
                 sign_up_date DATE NOT NULL,
                 last_login DATE NOT NULL
             )
@@ -157,9 +162,42 @@ class UserOperations:
     
     def add_user_type(self,user_type:str,limit_borrow_book:int):
         query="INSERT INTO user_types (type,limit_borrow) VALUES (%s,%s)"
-        self.cursor.execute(query,(user_type,limit_borrow_book))
-        self.connection.commit()
+        self.__cursor.execute(query,(user_type,limit_borrow_book))
+        self.__connection.commit()
 
     def remove_user_type(self,type_id:int):
         query="DELETE FROM user_types WHERE type_id=%s"
-        self.cursor.execute(query,(type_id,))
+        self.__cursor.execute(query,(type_id,))
+        self.__connection.commit()
+
+    def add_user(self,first_name:str,
+                last_name:str,
+                user_name:str,
+                type_id:int,
+                password:str,
+                age:int
+            ):
+        query="""
+            INSERT INTO users (
+                first_name,
+                last_name,
+                user_name,
+                type_id,
+                password,
+                age,
+                sign_up_date,
+                last_login
+            ) 
+            VALUES (%s,%s,%s,%s,%s,%s,NOW(),NOW())
+            """
+        self.__cursor.execute(
+            query,(
+                first_name,last_name,user_name,type_id,password,age
+                )
+            )
+        self.__connection.commit()
+
+    def remove_user(self,user_id:int):
+        query="DELETE FROM users WHERE user_id=%s"
+        self.__cursor.execute(query,(user_id,))
+        self.__connection.commit()
