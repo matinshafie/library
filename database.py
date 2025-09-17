818908c9b26597416a8e08e22159b9ece0df577b
matinshafie <matinshafie88@gmail.com>
2025-09-18 01:24:47 +0330

    changed the structure of code

---
+++ database.py
@@ -0,0 +1,290 @@
+import os
+import mysql.connector as sql
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_host = os.getenv('DB_HOST')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_name = os.getenv('DB_NAME')
+
+try:
+    conn = sql.connect(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        database=db_name
+    )
+    if conn.is_connected():
+        if __name__ == "__main__":
+            print("successfuly connected to mysql server")
+
+        cursor=conn.cursor()
+except:
+    if __name__=="__main__":
+        print("error not connected to mysql server")
+
+cursor.execute("USE simple_library")
+
+def add_book(author_id:int,title:str,number:int,published_year:str):
+    query="""
+    INSERT INTO books 
+        (author_id,title,number,published_year) 
+    VALUES (%s,%s,%s,%s)
+    """
+
+    cursor.execute(query, (author_id,title,number,published_year))
+    conn.commit()
+
+def add_author(first_name:str,last_name:str):
+    query="""
+    INSERT INTO authors 
+        (first_name,last_name) 
+    VALUES (%s,%s)
+    """
+
+    cursor.execute(query, (first_name,last_name))
+    conn.commit()
+
+def add_tag(tag:str):
+    cursor.execute(
+        "INSERT INTO tags (tag) VALUES (%s)",
+        (tag,)
+        )
+    
+def borrow_book(book_id:int,client_id:int):
+    cursor.execute(
+        "INSERT INTO borrowed_books (book_id,client_id) VALUES (%s,%s)",
+        (book_id,client_id)
+        )
+    conn.commit()
+
+def add_client(first_name:str,last_name:str,birth_date:str):
+    cursor.execute(
+        "INSERT INTO clients (first_name,last_name,birth_date) VALUES (%s,%s,%s)",
+        (first_name,last_name,birth_date)
+        )
+    conn.commit()
+
+def add_book_tag(book_id:int,tag_id:int):
+    cursor.execute(
+        "INSERT INTO book_tag (book_id,tag_id) VALUES (%s,%s)",
+        (book_id,tag_id)
+        )
+    conn.commit()
+    
+def remove_author(author_id:int):
+    cursor.execute(
+        "DELETE FROM authors WHERE author_id = %s",(author_id,)
+        )
+    conn.commit()
+    
+def remove_book(book_id:int):
+    cursor.execute("DELETE FROM books WHERE book_id=%s",(book_id,))
+    conn.commit()
+
+def remove_tag(tag_id:int):
+    cursor.execute("DELETE FROM tags WHERE tag_id=%s",(tag_id,))
+    conn.commit()
+
+def remove_borrowed_book(borrowed_id:int):
+    cursor.execute("DELETE FROM borrowed_books WHERE borrowed_id=%s",(borrowed_id,))
+    conn.commit()
+
+def remove_client(client_id:int):
+    cursor.execute("DELETE FROM clients WHERE client_id=%s",(client_id,))
+    conn.commit()
+
+def remove_book_tag(book_id:int,tag_id:int):
+    query="DELETE FROM book_tag WHERE book_id=%s AND tag_id=%s"
+    cursor.execute(query,(book_id,tag_id))
+    conn.commit()
+
+def search_books(
+        book_id:int=None,
+        author_id:int=None,
+        title:str=None,
+        published_year:str=None,
+        )->list[tuple]:
+    book_search_columns=[]
+    book_column_names=list[str]()
+    if book_id is not None:
+        book_search_columns.append(str(book_id))
+        book_column_names.append("book_id=%s")
+    if author_id is not None:
+        book_search_columns.append(str(author_id))
+        book_column_names.append("author_id=%s")
+    if title is not None:
+        book_search_columns.append(title)
+        book_column_names.append("title=%s")
+    if published_year is not None:
+        book_search_columns.append(published_year)
+        book_column_names.append("published_year=%s")
+
+    query="SELECT * FROM books"
+    if book_column_names:
+        query+=" WHERE "
+    query+=" AND ".join(book_column_names)
+    cursor.execute(query,tuple(book_search_columns))
+    return cursor.fetchall()
+
+def search_authors(
+        author_id:int=None,
+        first_name:str=None,
+        last_name:str=None,
+        )->list[tuple]:
+    author_search_columns=[]
+    author_column_names=list[str]()
+    if author_id is not None:
+        author_search_columns.append(str(author_id))
+        author_column_names.append("author_id=%s")
+    if first_name is not None:
+        author_search_columns.append(first_name)
+        author_column_names.append("first_name=%s")
+    if last_name is not None:
+        author_search_columns.append(last_name)
+        author_column_names.append("last_name=%s")
+
+    query="SELECT * FROM authors"
+    if author_column_names:
+        query+=" WHERE "
+    query+=" AND ".join(author_column_names)
+    cursor.execute(query,tuple(author_search_columns))
+    return cursor.fetchall()
+
+def search_book_tag(
+        book_id:int=None,
+        tag_id:int=None,
+        )->list[tuple]:
+    book_tag_search_columns=[]
+    book_tag_column_names=list[str]()
+    if book_id is not None:
+        book_tag_search_columns.append(str(book_id))
+        book_tag_column_names.append("book_id=%s")
+    if tag_id is not None:
+        book_tag_search_columns.append(str(tag_id))
+        book_tag_column_names.append("tag_id=%s")
+
+    query="SELECT * FROM book_tag"
+    if book_tag_column_names:
+        query+=" WHERE "
+    query+=" AND ".join(book_tag_column_names)
+    cursor.execute(query,tuple(book_tag_search_columns))
+    return cursor.fetchall()
+
+def search_book_tag(
+        book_id:int=None,
+        tag_id:int=None,
+        )->list[tuple]:
+    book_tag_search_columns=[]
+    book_tag_column_names=list[str]()
+    if book_id is not None:
+        book_tag_search_columns.append(str(book_id))
+        book_tag_column_names.append("book_id=%s")
+    if tag_id is not None:
+        book_tag_search_columns.append(str(tag_id))
+        book_tag_column_names.append("tag_id=%s")
+
+    query="SELECT * FROM book_tag"
+    if book_tag_column_names:
+        query+=" WHERE "
+    query+=" AND ".join(book_tag_column_names)
+    cursor.execute(query,tuple(book_tag_search_columns))
+    return cursor.fetchall()
+
+def search_tag(
+        tag_id:int=None,
+        tag:str=None,
+        )->list[tuple]:
+    tag_search_columns=[]
+    tag_column_names=list[str]()
+    if tag_id is not None:
+        tag_search_columns.append(str(tag_id))
+        tag_column_names.append("tag_id=%s")
+    if tag is not None:
+        tag_search_columns.append(tag)
+        tag_column_names.append("tag_id=%s")
+
+    query="SELECT * FROM tags"
+    if tag_column_names:
+        query+=" WHERE "
+    query+=" AND ".join(tag_column_names)
+    cursor.execute(query,tuple(tag_search_columns))
+    return cursor.fetchall()
+
+def search_borrowed_books(
+        borrowed_id:int=None,
+        book_id:int=None,
+        client_id:int=None
+        )->list[tuple]:
+    borrowed_books_search_columns=[]
+    borrowed_books_column_names=list[str]()
+    if borrowed_id is not None:
+        borrowed_books_search_columns.append(str(borrowed_id))
+        borrowed_books_column_names.append("borrowed_id=%s")
+    if book_id is not None:
+        borrowed_books_search_columns.append(str(book_id))
+        borrowed_books_column_names.append("book_id=%s")
+    if client_id is not None:
+        borrowed_books_search_columns.append(str(client_id))
+        borrowed_books_column_names.append("client_id=%s")
+
+    query="SELECT * FROM borrowed_books"
+    if borrowed_books_column_names:
+        query+=" WHERE "
+    query+=" AND ".join(borrowed_books_column_names)
+    cursor.execute(query,tuple(borrowed_books_search_columns))
+    return cursor.fetchall()
+
+def search_clients(
+        client_id:int=None,
+        first_name:str=None,
+        last_name:str=None,
+        birth_date:str=None
+        )->list[tuple]:
+    clients_search_columns=[]
+    clients_column_names=list[str]()
+    if client_id is not None:
+        clients_search_columns.append(str(client_id))
+        clients_column_names.append("client_id=%s")
+    if first_name is not None:
+        clients_search_columns.append(first_name)
+        clients_column_names.append("first_name=%s")
+    if last_name is not None:
+        clients_search_columns.append(last_name)
+        clients_column_names.append("last_name=%s")
+    if birth_date is not None:
+        clients_search_columns.append(birth_date)
+        clients_column_names.append("birth_date=%s")
+
+    query="SELECT * FROM clients"
+    if clients_column_names:
+        query+=" WHERE "
+    query+=" AND ".join(clients_column_names)
+    cursor.execute(query,tuple(clients_search_columns))
+    return cursor.fetchall()
+
+def book_id_exists(book_id:int)->bool:
+    cursor.execute("SELECT 1 FROM books WHERE book_id=%s",(book_id,))
+    return bool(cursor.fetchone())
+
+def author_id_exists(author_id:int)->bool:
+    cursor.execute("SELECT 1 FROM authors WHERE author_id=%s",(author_id,))
+    return bool(cursor.fetchone())
+
+def book_tag_id_exists(book_id:int,tag_id:int)->bool:
+    cursor.execute("SELECT 1 FROM book_tag b WHERE book_id=%s AND tag_id=%s",(book_id,tag_id))
+    return bool(cursor.fetchone())
+
+def borrowed_id_exists(borrowed_id:int)->bool:
+    cursor.execute("SELECT 1 FROM borrowed_books WHERE borrowed_id=%s",(borrowed_id,))
+    return bool(cursor.fetchone())
+
+def client_id_exists(client_id:int)->bool:
+    cursor.execute("SELECT 1 FROM clients WHERE client_id=%s",(client_id,))
+    return bool(cursor.fetchone())
+
+def tag_id_exists(tag_id:int)->bool:
+    cursor.execute("SELECT 1 FROM tags WHERE tag_id=%s",(tag_id,))
+    return bool(cursor.fetchone())
---
+++ ui.py
@@ -0,0 +1,302 @@
+import database as db
+from typing import Sequence
+from datetime import datetime
+
+
+def show_options():
+    print("1-book options")
+    print("2-author options")
+    print("3-book tag options")
+    print("4-tags options")
+    print("5-borrowed books options")
+    print("6-clients options")
+    print("7-exit")
+    print()
+
+def get_prompt(prompt:str,in_len:range=None,allowed_null:bool=False)->str:
+    while True:
+        user_in=input(prompt).strip()
+        if user_in=="" and allowed_null:
+            return None
+        if in_len is not None:
+            if len(user_in) in in_len:
+                return user_in
+        else:
+            return user_in
+        print("please enter a valid value try again")
+    
+
+def get_integer(prompt:str,num_range:range=None,allowed_null:bool=False)->int:
+    while True:
+        try:
+            user_in=input(prompt).strip()
+            if user_in=="" and allowed_null:
+                return None
+            user_in=int(user_in)
+            if num_range:
+                if user_in not in num_range:
+                    raise ValueError
+            return user_in
+        except ValueError:
+            print("please enter an integer value or your value should be listed on the options")
+            print("try again!")
+
+def get_date(prompt:str)->bool:
+    while True:
+        try:
+            return datetime.strptime(get_prompt(prompt),"%Y-%m-%d").date()
+        except:
+            print("enter date correctly (like 2000-01-01)")
+
+def print_squence(sequence:Sequence[tuple]):
+    for num,row in enumerate(sequence,1):
+        print(f"{num}-{row}")
+    print()
+            
+def show_book_options():
+    print("1-add book")
+    print("2-remove book")
+    print("3-search book")
+    print("4-back")
+
+def show_author_options():
+    print("1-add author")
+    print("2-remove author")
+    print("3-search author")
+    print("4-back")
+
+def show_book_tag_options():
+    print("1-add book_tag")
+    print("2-remove book_tag")
+    print("3-search book_tag")
+    print("4-back")
+
+def show_tag_options():
+    print("1-add tag")
+    print("2-remove tag")
+    print("3-search tag")
+    print("4-back")
+
+def show_borrowed_books_options():
+    print("1-add borrowed books")
+    print("2-remove borrowed books")
+    print("3-search borrowed books")
+    print("4-back")
+
+def show_client_options():
+    print("1-add client")
+    print("2-remove client")
+    print("3-search client")
+    print("4-back")
+
+def add_book():
+    title=get_prompt("enter title: ",range(1,100)).strip()
+    author_id=get_integer("enter author id: ")
+    number=get_integer("enter the number of books: ")
+    published_year=get_date("enter published date year: ")
+
+    db.add_book(author_id,title,number,published_year)
+
+def search_books():
+    book_id=get_integer("enter book id: (or skip)",allowed_null=True)
+    author_id=get_integer("enter author id: (or skip)",allowed_null=True)
+    title=get_prompt("enter title: (or skip)",allowed_null=True)
+    published_year=get_prompt("enter published year: (or skip)",allowed_null=True)
+    print_squence(db.search_books(book_id,author_id,title,published_year))
+
+def remove_book():
+    book_id=get_integer("enter book id: ")
+    if db.book_id_exists(book_id):
+        db.remove_book(book_id)
+    else:
+        print("book id doesn't exist")
+
+def book_options():
+    show_book_options()
+    option=get_integer("enter option number: ",range(1,5))
+    if option==1:
+        add_book()
+    elif option==2:
+        remove_book()
+    elif option==3:
+        search_books()
+    elif option==4:
+        pass
+
+def add_author():
+    first_name=get_prompt("enter the first name: ",range(1,50)).strip()
+    last_name=get_prompt("enter the last name: ",range(1,50)).strip()
+
+    db.add_author(first_name,last_name)
+
+def search_authors():
+    author_id=get_integer("enter author id: (or skip)",allowed_null=True)
+    first_name=get_prompt("enter first name: (or skip)",allowed_null=True)
+    last_name=get_prompt("enter last name: (or skip)",allowed_null=True)
+    print_squence(db.search_authors(first_name,author_id,last_name))
+
+def remove_author():
+    author_id=get_integer("enter author id: ")
+    if db.author_id_exists(author_id):
+        db.remove_author(author_id)
+    else:
+        print("author id doesn't exist")
+
+def author_options():
+    show_author_options()
+    option=get_integer("enter option number: ",range(1,5))
+    if option==1:
+        add_author()
+    elif option==2:
+        remove_author()
+    elif option==3:
+        search_authors()
+    elif option==4:
+        pass
+
+def add_book_tag():
+    book_id=get_integer("enter book id: ")
+    tag_id=get_integer("enter tag id: ")
+
+    db.add_book_tag(book_id,tag_id)
+
+def search_book_tags():
+    book_id=get_integer("enter book id: (or skip)",allowed_null=True)
+    tag_id=get_integer("enter tag id: (or skip)",allowed_null=True)
+    print_squence(db.search_book_tag(book_id,tag_id))
+
+def remove_book_tag():
+    book_id=get_integer("enter book id: ")
+    tag_id=get_integer("enter tag id: ")
+    if db.book_tag_id_exists(book_id,tag_id):
+        db.remove_book_tag(book_id,tag_id)
+    else:
+        print("book tag ids doesn't exist")
+
+def book_tag_options():
+    show_book_tag_options()
+    option=get_integer("enter option number: ",range(1,5))
+    if option==1:
+        add_book_tag()
+    elif option==2:
+        remove_book_tag()
+    elif option==3:
+        search_book_tags()
+    elif option==4:
+        pass
+
+def add_tag():
+    tag=get_prompt("enter book tag name: ",range(1,50)).strip()
+
+    db.add_tag(tag)
+
+def search_tag():
+    tag_id=get_integer("enter tag id: (or skip)",allowed_null=True)
+    tag=get_prompt("enter tag: (or skip)",allowed_null=True)
+    print_squence(db.search_tag(tag_id,tag))
+
+def remove_tag():
+    tag_id=get_integer("enter tag id: ")
+    if db.tag_id_exists(tag_id):
+        db.remove_tag(tag_id)
+    else:
+        print("tag id doesn't exist")
+
+def tag_options():
+    show_tag_options()
+    option=get_integer("enter option number: ",range(1,5))
+    if option==1:
+        add_tag()
+    elif option==2:
+        remove_tag()
+    elif option==3:
+        search_tag()
+    elif option==4:
+        pass
+
+def borrow_book():
+    book_id=get_prompt("enter book id: ").strip()
+    client_id=get_prompt("enter client id: ").strip()
+
+    db.borrow_book(book_id,client_id)
+
+def search_borrowed_books():
+    borrowed_id=get_integer("enter borrowed id: (or skip)",allowed_null=True)
+    book_id=get_integer("enter book id: (or skip)",allowed_null=True)
+    client_id=get_integer("enter client id: (or skip)",allowed_null=True)
+    print_squence(db.search_borrowed_books(borrowed_id,book_id,client_id))
+
+def remove_borrowed_book():
+    borrowed_id=get_integer("enter borrowed id: ")
+    if db.borrowed_id_exists(borrowed_id):
+        db.remove_borrowed_book(borrowed_id)
+    else:
+        print("borrowed id doesn't exist")
+
+def borrow_book_options():
+    show_borrowed_books_options()
+    option=get_integer("enter option number: ",range(1,5))
+    if option==1:
+        borrow_book()
+    elif option==2:
+        remove_borrowed_book()
+    elif option==3:
+        search_borrowed_books()
+    elif option==4:
+        pass
+
+def add_client():
+    first_name=get_prompt("enter the first name: ").strip()
+    last_name=get_prompt("enter the last name: ").strip()
+    birth_date=get_date("enter the birth date year: ").strip()
+
+    db.add_client(first_name,last_name,birth_date)
+
+def search_clients():
+    client_id=get_integer("enter client_id id: (or skip)",allowed_null=True)
+    first_name=get_prompt("enter first name: (or skip)",allowed_null=True)
+    last_name=get_prompt("enter last name: (or skip)",allowed_null=True)
+    print_squence(db.search_clients(client_id,first_name,last_name))
+
+def remove_client():
+    client_id=get_integer("enter client id: ")
+    if db.client_id_exists(client_id):
+        db.remove_client(client_id)
+    else:
+        print("client id doesn't exist")
+
+def client_options():
+    show_client_options()
+    option=get_integer("enter option number: ",range(1,5))
+    if option==1:
+        add_client()
+    elif option==2:
+        client_id=get_integer("enter client id: ")
+        db.remove_client(client_id)
+    elif option==3:
+        search_clients()
+    elif option==4:
+        pass
+
+def run_library():
+    while True:
+        show_options()
+        option=get_integer("please enter option number: ",range(1,8))
+
+        if option==1:
+            book_options()
+        elif option==2:
+            author_options()
+        elif option==3:
+            book_tag_options()
+        elif option==4:
+            tag_options()
+        elif option==5:
+            borrow_book_options()
+        elif option==6:
+            client_options()
+        elif option==7:
+            break
+
+if __name__=="__main__":
+    run_library()
